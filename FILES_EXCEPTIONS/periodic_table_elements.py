a19ab7260683384a68bcd6867a3fa197904ab0dc
AJ <amarjits1@hotmail.com>
2021-01-15 17:35:34 -0700

    worked on files and exceptions

---
+++ FILES_EXCEPTIONS/periodic_table_elements.py
@@ -0,0 +1,39 @@
+# Exercise 161: What's the Element?
+
+def get_data():
+    file = open("elements.txt", mode="r")
+    lines = list(map(lambda x: x.strip().split(","), file.readlines()))
+
+    num_to_symbol_name = {}
+    for info in lines:
+        num_to_symbol_name[int(info[0])] = tuple(info[1:])
+
+    symbol_name_to_num = {}
+    for key in num_to_symbol_name:
+        symbol_name_to_num[num_to_symbol_name[key]] = key
+
+    file.close()
+    return num_to_symbol_name, symbol_name_to_num
+
+
+def main():
+    num_keys, symbol_keys = get_data()
+    user_input = input("What do you want to know about?  ").capitalize()
+
+    while user_input != "":
+        try:
+            x = int(user_input)
+            print(num_keys[x])
+        except ValueError:
+            for key in symbol_keys:
+                if key[0] == user_input or key[1] == user_input:
+                    print(symbol_keys[key])
+                    break
+            else:
+                print("No such element on periodic table.")
+        except KeyError:
+            print("No element with that atomic number.")
+        user_input = input("\nWhat do you want to know about?  ").capitalize()
+
+
+main()
--- FILES_EXCEPTIONS/weird_I_E_words.py
+++ FILES_EXCEPTIONS/weird_I_E_words.py
@@ -28,14 +28,110 @@ def check_ie_rule(words):
 
     for word in words:
 
+        #### 3872 = yes
+        #### 2763 = no
+        # Skip any word not containing "ei" or "ie"
+        if "ei" not in word and "ie" not in word:
+            continue
+
+        # Only need to check for "cie"
+        # Otherwise, having "ie" is fine.
         if "cie" in word:
             no.write(word+"\n")
-        elif "cei" in word:
+            continue
+
+        # Now it remains to investigate "cei" and "ei"
+        index = word.find("ei")
+        if index == -1:  # There is no "ei"
             yes.write(word+"\n")
-        elif "ie" in word:
+            continue
+
+        while index != -1:  # There is at least one "ei"
+            if word[index-1] != "c":
+                no.write(word+"\n")
+                break
+            else:  # Check remainder of word
+                index = word[index+1:].find("ei")
+        else:  # There is no "ei" without "c" before it.
             yes.write(word+"\n")
-        elif "ei" in word:
-            no.write(word+"\n")
+
+
+
+
+
+        #### 3868 = yes
+        #### 2735 = no
+        # if "ie" in word or "ei" in word:
+        #
+        #     if "ie" in word and "ei" not in word:
+        #         if "cie" in word:
+        #             no.write(word+"\n")
+        #         else:
+        #             yes.write(word+"\n")
+        #
+        #     if "ie" not in word and "ei" in word:
+        #         if "cei" in word:
+        #             yes.write(word+"\n")
+        #         else:
+        #             no.write(word+"\n")
+
+
+
+        #### 4157 = yes
+        #### 2735 = no
+        # if "cie" in word:
+        #     no.write(word + "\n")
+        # if "ie" in word:
+        #     yes.write(word+"\n")
+        # elif "cei" in word:
+        #     yes.write(word+"\n")
+        # elif "ei" in word:
+        #     no.write(word+"\n")
+
+        #### 3713 = yes
+        #### 3366 = no
+        # if "ie" in word or "cei" in word:  # Its good so far!
+        #     if "cie" in word or "ei" in word:
+        #         no.write(word+"\n")
+        #     else:
+        #         yes.write(word+"\n")
+        #
+        # if "cie" in word or "ei" in word:
+        #     no.write(word+"\n")
+
+        #### 3900 = yes
+        #### 2735 = no
+        # if "cie" in word:
+        #     no.write(word + "\n")
+        # elif "ie" in word:
+        #     yes.write(word+"\n")
+        # elif "cei" in word:
+        #     yes.write(word+"\n")
+        # elif "ei" in word:
+        #     no.write(word+"\n")
+
+
+
+        #### 233002 = yes
+        #### 2922 = no
+        #
+        # if "ie" in word:
+        #     boom = True
+        # if "cie" in word:
+        #     boom = False
+        #
+        # if "ei" in word:
+        #     boom = False
+        # if "cei" in word:
+        #     boom = True
+        #
+        #
+        # if boom:
+        #     yes.write(word+"\n")
+        # else:
+        #     no.write(word+"\n")
+
+
 
     yes.close()
     no.close()
