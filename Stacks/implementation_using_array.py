3b132480f89cbc1ea28b0743eab7e89a8197ee3d
AJ <amarjits1@hotmail.com>
2021-02-22 16:47:18 -0700

    implemented a stack via an array

--- Stacks/implementation_using_array.py
+++ Stacks/implementation_using_array.py
@@ -11,36 +11,35 @@ class Empty(Exception):
 
 
 class ArrayStack:
-    """Implementation of a stack using an array."""
+    """Implementation of a LIFO stack using an array."""
 
     def __init__(self):
         """Create a new empty stack."""
-
         self._stack = []
-        self._n = 0
 
     def push(self, e):
         """Add a new element 'e' to the top of the stack."""
-        self._n += 1
         self._stack.append(e)
 
     def pop(self):
         """Remove and return the top element of the stack."""
 
-        if self._n > 0:
-            self._n -= 1
-            return self._stack.pop()
-        raise Empty
+        if self.is_empty():
+            raise Empty("The stack contains no elements.")
+        return self._stack.pop()
 
     def top(self):
         """Returns a reference to the top element of a stack."""
+
+        if self.is_empty():
+            raise Empty("The stack contains no elements.")
         return self._stack[-1]
 
     def is_empty(self):
         """Check whether a stack contains any elements."""
-        return self._n == 0
+        return len(self._stack) == 0
 
     def __len__(self):
         """Return the number of elements in a stack."""
-        return self._n
+        return len(self._stack)
 
