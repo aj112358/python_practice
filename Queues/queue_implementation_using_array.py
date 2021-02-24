8e8e8f79b9ba1acf1c80744fbd3a6ccd310c2ffd
AJ <amarjits1@hotmail.com>
2021-02-23 23:51:35 -0700

    implementation of a queue via arrays

--- Queues/queue_implementation_using_array.py
+++ Queues/queue_implementation_using_array.py
@@ -1,4 +1,4 @@
-"""An implementation of a queue with an array as the underlying structure.
+"""An implementation of a queue with an array as the underlying structure, resizing when needed.
 
 Created By: AJ Singh
 Date: Feb 23, 2021
@@ -11,16 +11,14 @@ class Empty(Exception):
 
 
 class ArrayQueue:
-    """An implementation of a queue using arrays."""
+    """An implementation of a queue following the FIFO principle using arrays."""
 
-    def __init__(self, N=10, size=0, front=0):
-        """Instantiating a new queue."""
+    def __init__(self, n=10):
+        """Instantiating a new queue, with default size 10."""
 
-        self._N = N
-
-        self._data = [] * N  # List instance to store the queue elements.
-        self._size = size    # Integer representing the actual number of elements in the queue.
-        self._front = front  # Integer representing the index of the 'first' queue element.
+        self._data = [None] * n  # List instance to store the queue elements.
+        self._size = 0           # Actual number of elements in the queue.
+        self._front = 0          # Index of the 'first' queue element.
 
     def __len__(self):
         """Return the actual number of elements in the queue."""
@@ -31,7 +29,9 @@ class ArrayQueue:
         return self._size == 0
 
     def first(self):
-        """Returns the element at the front of the queue."""
+        """Returns (but does not remove) the element at the front of the queue."""
+        if self.is_empty():
+            raise Empty("The queue contains no elements.")
         return self._data[self._front]
 
     def dequeue(self):
@@ -40,22 +40,80 @@ class ArrayQueue:
         if self.is_empty():
             raise Empty("The queue contains no elements.")
 
-        elem = self._data[self._front]
+        elem = self._data[self._front]  # Save element for returning (before removal from queue).
+
         self._size -= 1
-        self._data[self._front] = None
-        self._front = (self._front + 1) % self._N
+        self._data[self._front] = None  # Remove element from the queue.
+        self._front = (self._front + 1) % len(self._data)
+
+        # Shrink size of underlying array if there are few enough elements in queue.
+        if self._size <= len(self._data) // 4:
+            self._resize(len(self._data) // 2)
 
         return elem
 
     def enqueue(self, e):
         """Adds element 'e' to the back of the queue."""
 
-        if self._size == self._N:
-            self.resize()
+        # Enlarge underlying array if current queue is full.
+        if self._size == len(self._data):
+            self._resize(self._size * 2)
 
-        back = (self._front + self._size) % self._N
+        back = (self._front + self._size) % len(self._data)
         self._data[back] = e
+        self._size += 1
 
-    def resize(self):
-        pass
+    def _resize(self, capacity):
+        """Resize the underlying array to have size 'capacity'."""
 
+        temp = [None] * capacity  # New array to store queue elements.
+
+        for k in range(self._size):
+            temp[k] = self._data[self._front % len(self._data)]  # Copy queue elements into new array.
+            self._front = (self._front + 1) % len(self._data)
+
+        self._front = 0
+        self._data = temp
+
+    def __str__(self):
+        """Prints a human-readable representation of the queue."""
+        return " < ".join(str(_) for _ in self._data)
+
+
+if __name__ == "__main__":
+    print()
+
+    q = ArrayQueue(n=5)
+
+    q.enqueue("A")
+    q.enqueue("B")
+    q.enqueue("C")
+    q.enqueue("D")
+    print(q)
+
+    q.dequeue()
+    print(q)
+
+    q.enqueue("E")
+    print(q)
+
+    q.enqueue("F")
+    print(q)
+
+    q.dequeue()
+    print(q)
+
+    q.enqueue("G")
+    q.enqueue("H")
+    print(q)
+
+    q.dequeue()
+    q.dequeue()
+    q.dequeue()
+    print(q)
+
+    q.dequeue()
+    print(q)
+
+    q.dequeue()
+    print(q)
