57a3d1226e65add46180110203e123d227006994
AJ <amarjits1@hotmail.com>
2021-02-25 22:25:44 -0700

    need to think about math formulas to make polygons well-defined:

--- (2)_OOP/polygon_class.py
+++ (2)_OOP/polygon_class.py
@@ -7,6 +7,8 @@ For now, we assume only *regular* polygons:
     - Hexagon
     - Octagon
 
+TO-DO: For each shape, write down various ways to make it well-defined.
+
 Created By: AJ Singh
 Date: Feb 25, 2021
 """
@@ -18,9 +20,9 @@ from abc import abstractmethod
 class Polygon:
     """A class to create a general polygon, meant to specify the required methods for sub-classes."""
 
-    def __init__(self, l):
+    def __init__(self):
         """Create a new regular polygon with side length l."""
-        self._length = l
+        # self._length = l
 
     @abstractmethod
     def area(self):
@@ -34,21 +36,24 @@ class Polygon:
 class Triangle(Polygon):
     """Class to create a triangle."""
 
-    def __init__(self, n):
+    def __init__(self, base, height):
         """Construct a new triangle."""
-        super().__init__(n)
+        super().__init__()
+        self._base = base
+        self._height = height
 
     def area(self):
         """Compute area of a triangle."""
-        return sqrt(3) / 4 * self._length**2
+        # return sqrt(3) / 4 * self._length**2
+        return 0.5 * self._base * self._height
 
     def perimeter(self):
         """Compute the perimeter of a triangle."""
-        return 3 * self._length
+        return "hello"
 
 
 if __name__ == "__main__":
 
-    triangle = Triangle(2)
+    triangle = Triangle(3,5)
     print(triangle.area())
     print(triangle.perimeter())
