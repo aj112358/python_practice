1d90f1955cc33a6d1925db87a4269e3382a8c19e
AJ <amarjits1@hotmail.com>
2021-03-01 17:28:07 -0700

    implemented queue using a linked list

--- Linked_Lists/queue_via_linked_list.py
+++ Linked_Lists/queue_via_linked_list.py
@@ -22,24 +22,68 @@ class LinkedQueue:
 
     def __init__(self):
         """Instantiating an empty queue."""
-
-
+        self._head = None   # Reference to FRONT of the queue.
+        self._tail = None   # Reference to BACK of the queue.
+        self._size = 0      # Maintains number of elements in the queue.
 
     def __len__(self):
-        pass
+        """Return the number of elements in the queue."""
+        return self._size
 
     def is_empty(self):
-        pass
+        """Return True if no elements in queue; else return False."""
+        return self._size == 0
 
     def first(self):
-        pass
+        """Return (but not remove) the current first element in the queue."""
+        if self._size == 0:
+            raise Empty("No elements in queue.")
+        return self._head._element
 
     def enqueue(self, e):
-        pass
+        """Add new element 'e' to back of the queue (ie. tail of the linked list)."""
+        new = self._Node(e, None)
+
+        if self._size == 0:
+            self._head = new
+        else:
+            self._tail._next = new  # If queue is not empty, re-assign old tail reference to reference the new node.
+
+        self._tail = new  # Re-assign tail as new node.
+        self._size += 1
 
     def dequeue(self):
-        pass
+        """Remove and return the first element in queue (ie. head of linked list)."""
+        if self._size == 0:
+            raise Empty("No elements in queue.")
+
+        elem = self._head._element
+        self._head = self._head._next  # Will become 'None' if queue is now empty!
+        self._size -= 1
+
+        if self._size == 0:
+            self._tail = None  # Need to manually make the tail be 'None' if queue is now empty!
+
+        return elem
 
 
 if __name__ == "__main__":
-    print("Hello World")
+
+    q = LinkedQueue()
+    print(len(q))
+
+    q.enqueue("First")
+    q.enqueue("Second")
+    print(len(q))
+    print(q.first())
+    print()
+
+    q.enqueue("Third")
+    print(q.dequeue())
+    print(q.dequeue())
+    print(q.is_empty())
+    print()
+
+    print(q.dequeue())
+    print(q.is_empty())
+    print(q.first())
