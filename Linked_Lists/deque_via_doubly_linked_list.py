a08babdaf7343975013483426ae2ddb99b5ccb7e
AJ <amarjits1@hotmail.com>
2021-03-04 16:28:09 -0700

    implementing deque ADT using a doubly linked list

---
+++ Linked_Lists/deque_via_doubly_linked_list.py
@@ -0,0 +1,31 @@
+"""This is an implementation of a deque using a doubly linked list as the underlying structure.
+
+Created By: AJ Singh
+Date: March 4, 2021
+"""
+
+from doubly_linked_list_base_class import _DoublyLinkedBase
+
+
+class LinkedDeque(_DoublyLinkedBase):
+    """An implementation of a deque ADT, using a doubly linked list."""
+
+    def first(self):
+
+
+    def last(self):
+
+
+
+    def insert_first(self):
+
+
+    def insert_last(self):
+
+
+
+    def delete_first(self):
+
+
+    def delete_last(self):
+        
