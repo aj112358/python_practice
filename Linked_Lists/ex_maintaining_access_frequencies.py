3f79b049048d2d92ea9571c51660fc27afdf7c46
AJ <amarjits1@hotmail.com>
2021-03-07 15:40:16 -0700

    implemented non-public utilities, _find_position & _move_up

--- Linked_Lists/ex_maintaining_access_frequencies.py
+++ Linked_Lists/ex_maintaining_access_frequencies.py
@@ -13,21 +13,39 @@ class FavoritesList:
     # ----- Class to store an element's value and access frequency. ----- #
     class _Item:
         """Object to store elements' value and access counts."""
-
+        __slots__ = "_value", "_count"
         def __init__(self, e):
             self._value = e  # Element.
             self._count = 0  # To store access count.
 
-    # ----- Non-public Utilities ----- #
+    # ----- Non-Public Utilities ----- #
     def _find_position(self, e):
-        pass
+        """Search for element 'e' and return its position, or None if not found."""
+
+        walk = self._data.first()  # Start at header of underlying linked list.
+        while (walk is not None) and (walk.element()._value != e):
+            walk = self._data.after(walk)
+        return walk
 
     def _move_up(self, p):
-        pass
+        """Move item at position 'p' *earlier* in the list, based on access counts."""
+
+        if p != self._data.first():
+
+            count = p.element()._count()
+            walk = self._data.before(p)
+            if count > walk.element()._count:  # Need to shift element forward/up the positional list.
+
+                # You're not at the head of the LL & Current count is larger than next count.
+                while (walk != self._data.first()) and (count > self._data.before(walk).element()._count):
+                    walk = self._data.before(walk)  # Continue backward traversal through linked list.
+
+                self._data.add_before(walk, self._data.delete(p))  # Insert element at correct position in list.
 
     # ----- Public Methods ----- #
     def __init__(self):
-        pass
+        """Create an empty list of favorites."""
+        self._data = PositionalList()  # Will be a list of _Item instances (from nested class above)
 
     def __len__(self):
         pass
@@ -43,4 +61,3 @@ class FavoritesList:
 
     def top(self, k):
         pass
-
