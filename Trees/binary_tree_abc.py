139c4023b1441806a9ebaf7b97a423d13168239a
AJ <amarjits1@hotmail.com>
2021-03-10 15:46:38 -0700

    coded .sibling() method

--- Trees/binary_tree_abc.py
+++ Trees/binary_tree_abc.py
@@ -20,4 +20,18 @@ class BinaryTree(Tree):
 
     # ----- Concrete methods ----- #
 
-    
+    def sibling(self, p):
+        """Return the position that represents the sibling of node 'p', or None if 'p' has no siblings."""
+        parent = self.parent(p)
+        if parent is None:  # p is the root of the tree, hence has no siblings.
+            return None
+
+        if p == self.left(parent):
+            return self.right(parent)
+        else:
+            return self.left(parent)
+
+        # for child in self.children(parent):
+        #     if p != child:
+        #         return child
+        # return None
