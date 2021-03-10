fc0437a910b0127b418cee85685dd80397db6b9f
AJ <amarjits1@hotmail.com>
2021-03-10 15:32:07 -0700

    implementing abstract base class for binary trees

---
+++ Trees/binary_tree_abc.py
@@ -0,0 +1,15 @@
+"""This module contains the abstract class for binary trees.
+
+Created By: AJ Singh
+Date: March 10, 2021
+"""
+
+from tree_abc import Tree
+
+
+class BinaryTree(Tree):
+    """An abstract base class for binary trees."""
+
+    # ----- Abstract methods, specific for binary trees. ----- #
+
+    
