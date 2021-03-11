3da54d63080214973013445f63dc284fbf5a4a4c
AJ <amarjits1@hotmail.com>
2021-03-11 15:49:54 -0700

    implementing a binary tree using linked lists

---
+++ Trees/linked_binary_tree.py
@@ -0,0 +1,13 @@
+"""A module that defines a binary tree using a linked structure, adapted from book.
+
+Created By: AJ Singh
+Date: March 11, 2021
+"""
+
+from binary_tree_abc import BinaryTree
+
+
+class LinkedBinaryTree(BinaryTree):
+    """A binary tree structure using a linked representation."""
+
+    
