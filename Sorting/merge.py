ac13fe1aa8ca4f2681984306d248f5250abd026f
AJ <amarjits1@hotmail.com>
2021-03-22 17:02:41 -0600

    refactoring merge function

--- Sorting/merge.py
+++ Sorting/merge.py
@@ -14,8 +14,13 @@ def merge(s1, s2, s=None):
     i = j = 0
     while i + j < len(s1) + len(s2):
 
-        # Not all s1 elements have been used.
-        if i < len(s1) and (j == len(s2) or (s1[i] < s2[j])):
+        # # Not all s1 elements have been used.
+        # if i < len(s1) and (j == len(s2) or (s1[i] < s2[j])):
+        #     s[i + j] = s1[i]
+        #     i += 1
+
+        # This is better than above due to short-circuiting!
+        if j == len(s2) or (i < len(s1) and s1[i] < s2[j]):
             s[i + j] = s1[i]
             i += 1
 
@@ -36,8 +41,8 @@ if __name__ == "__main__":
     s2 = [3, 9, 10, 18]
     print(*merge(s1, s2), sep=" ")
 
-    s1 = [2, 5, 8, 11, 15]
-    s2 = [3, 9, 10, 18]
+    s1 = [2, 5, 8, 11, 18]
+    s2 = [3, 9, 10, 20]
     print(*merge(s1, s2), sep=" ")
 
     s1 = [2, 5, 8, 20]
@@ -46,4 +51,8 @@ if __name__ == "__main__":
 
     s1 = [2, 5, 8, 11]
     s2 = [3, 9, 10, 18]
+    print(*merge(s1, s2), sep=" ")
+
+    s1 = [2, 5, 8, 11]
+    s2 = [3, 5, 10, 11]
     print(*merge(s1, s2), sep=" ")
