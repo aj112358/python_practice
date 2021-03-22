ca3db3f5a20bd99e1469c243fd79c2e898b603b1
AJ <amarjits1@hotmail.com>
2021-03-22 16:19:37 -0600

    working on base merge function for merge-sort

---
+++ Sorting/merge.py
@@ -0,0 +1,54 @@
+"""This module provides the basic function that merges already-sorted arrays.
+
+Created By: AJ Singh
+Date: March 22, 2021
+"""
+
+
+def merge(s1, s2, s=None):
+    """Given sorted arrays s1 & s2, merge them together into array s."""
+
+    if s is None:
+        s = [None] * (len(s1) + len(s2))
+
+    i = j = 0
+    while i + j < len(s1) + len(s2):
+
+        if i < len(s1) and j < len(s2):
+            if s1[i] < s2[j]:
+                s[i + j] = s1[i]
+                i += 1
+            else:
+                s[i+j] = s2[j]
+                j += 1
+        elif i < len(s1):
+            s[i + j] = s1[i]
+            i += 1
+        else:
+            s[i + j] = s2[j]
+            j += 1
+
+    if s is None:
+        return s
+
+
+if __name__ == "__main__":
+    s1 = [2, 5, 8, 11]
+    s2 = [3, 9, 10, 18, 20]
+    print(*merge(s1, s2), sep=" ")
+
+    s1 = [2, 5, 8, 11, 20]
+    s2 = [3, 9, 10, 18]
+    print(*merge(s1, s2), sep=" ")
+
+    s1 = [2, 5, 8, 11, 15]
+    s2 = [3, 9, 10, 18]
+    print(*merge(s1, s2), sep=" ")
+
+    s1 = [2, 5, 8, 20]
+    s2 = [3, 9, 10, 11, 18]
+    print(*merge(s1, s2), sep=" ")
+
+    s1 = [2, 5, 8, 11]
+    s2 = [3, 9, 10, 18]
+    print(*merge(s1, s2), sep=" ")
