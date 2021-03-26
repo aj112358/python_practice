d00a17c1374ae84f8de4eb180d5c9f4cb4505503
AJ <amarjits1@hotmail.com>
2021-03-26 16:27:23 -0600

    implementing queue-based quick-sort algorithm

---
+++ Sorting/quick_sort_queue.py
@@ -0,0 +1,11 @@
+"""An implementation of the quick-sort algorithm that operates on any queue-based sequence.
+
+Created By: AJ Singh
+Date: March 26, 2021
+"""
+
+
+def quick_sort(s):
+    """Sorts the elements of the queue 's' using the quick-sort algorithm."""
+
+    
