a34ac427520f4bd8d11f0c05788d1f651d9d0350
AJ <amarjits1@hotmail.com>
2021-03-26 17:11:25 -0600

    coding concatenation of subsequences

--- Sorting/quick_sort_queue.py
+++ Sorting/quick_sort_queue.py
@@ -11,7 +11,7 @@ def quick_sort(s):
     """Sorts the elements of the queue 's' using the quick-sort algorithm."""
 
     if len(s) in {0, 1}:
-        return s
+        return
 
     pivot = s.first()
 
@@ -20,9 +20,7 @@ def quick_sort(s):
     greater = LinkedQueue()
 
     elem = s.first()
-
-    while elem is not None:
-
+    while not s.is_empty():
         if elem < pivot:
             less.enqueue(s.dequeue())
         elif elem > pivot:
@@ -30,4 +28,41 @@ def quick_sort(s):
         else:
             equal.enqueue(s.dequeue())
 
-        elem = elem.next()
+        if len(s) == 0:
+            break
+        elem = s.first()
+
+    quick_sort(less)
+    quick_sort(greater)
+
+    while len(less) != 0:
+        s.enqueue(less.dequeue())
+    while len(equal) != 0:
+        s.enqueue(equal.dequeue())
+    while len(greater) != 0:
+        s.enqueue(greater.dequeue())
+
+    print("HellO", len(s))
+
+    # while len(s) != 0:
+    #     print(s.dequeue(), end=" ")
+
+    # return s
+
+
+if __name__ == "__main__":
+    seq = LinkedQueue()
+    elements = [50, 85, 24, 63, 45, 17, 31, 96]
+
+    for _ in elements:
+        seq.enqueue(_)
+
+    print("Original:")
+    while len(seq) != 0:
+        print(seq.dequeue(), end=" ")
+
+    quick_sort(seq)
+
+    print("\n\nSorted:")
+    while len(seq) != 0:
+        print(seq.dequeue(), end=" ")
