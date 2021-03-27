95ebfcafbfdbbbe5d973e574963c03179b04e23b
AJ <amarjits1@hotmail.com>
2021-03-27 16:56:04 -0600

    in-place implementation of quick-sort using arrays

---
+++ Sorting/quick_sort_inplace.py
@@ -0,0 +1,11 @@
+"""An implementation of the quick-sort algorithm as an in-place algorithm via arrays.
+
+Created By: AJ Singh
+Date: March 27, 2021
+"""
+
+
+def quick_sort_inplace(s, a, b):
+    """Sort an array 's' between indices a & b (inclusive) in-place using the quick-sort algorithm."""
+
+    
