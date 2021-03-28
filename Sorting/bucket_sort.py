61041afea0cf1bb4e33de74f7cb664d996481ec9
AJ <amarjits1@hotmail.com>
2021-03-28 17:31:28 -0600

    implemented bucket-sort algorithm for basic arrays

--- Sorting/bucket_sort.py
+++ Sorting/bucket_sort.py
@@ -1,6 +1,6 @@
 """An implementation of the bucket-sort algorithm.
 
-Entries of the input sequence are key-value pairs.
+Entries of the input sequence are integers, and we use the indices as the keys.
 
 Created By: AJ Singh
 Date: March 28, 2021
@@ -10,25 +10,25 @@ from math import floor
 
 
 def bucket_sort(s, k):
-    """Sort a sequence 's' in non-decreasing order using the bucket-sort algorithmm"""
+    """Sort a sequence 's' in non-decreasing order using the bucket-sort algorithm"""
 
     b = [[] for _ in range(k)]  # Setting up k empty buckets.
-    # m = len(s) - 1
-    m = max(s)+1
+    m = max(s) + 1
+
     for i in range(len(s)):
-        x = floor(k * s[i] / m)
-        print(x)
-        b[floor(k * s[i] / m)].append(s[i])
+        index = floor(k * s[i] / m)
+        b[index].append(s[i])
 
     for i in range(k):
-        b[i].sort()
+        b[i].sort()  # Can use any sorting algorithm for individual buckets.
 
-    return b
+    return [b[i][j] for i in range(k) for j in range(len(b[i]))]
 
 
 if __name__ == "__main__":
-    seq = [29, 25, 3, 49, 9, 37, 21, 43]
+    seq = [29, 25, 4, 49, 9, 37, 21, 43]
+    print("Original:")
+    print(*seq, sep=" ")
 
-
-
+    print("\nSorted:")
     print(bucket_sort(seq, 5))
