fc47b16ad3dbcfaa6510a5b5f8f43802a79786a1
AJ <amarjits1@hotmail.com>
2021-03-28 16:40:51 -0600

    writing code for bucket-sort algorithm

---
+++ Sorting/bucket_sort.py
@@ -0,0 +1,10 @@
+"""An implementation of the bucket-sort algorithm.
+
+Created By: AJ Singh
+Date: March 28, 2021
+"""
+
+
+def bucket_sort(s):
+    """Sort a sequence 's' in non-decreasing order using the bucket-sort algorithmm"""
+    
