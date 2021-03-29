1e93b9d8981ec708cbbc2919c17e2066c0dd5603
AJ <amarjits1@hotmail.com>
2021-03-29 15:46:36 -0600

    implementing Python's built-in sorting function's 'key' manually

---
+++ Sorting/decorated_merge_sort.py
@@ -0,0 +1,11 @@
+"""An implementation of the keyword argument 'key=' used in Python's built-in sorting functions, taken from book.
+
+An illustration of the 'decorate-sort-undecorate' design pattern.
+
+Created By: AJ Singh
+Date: March 29, 2021
+"""
+
+
+def decorated_merge_sort(data, key=None):
+    """Demonstration of the 'decorate-sort-undecorate' design pattern."""
