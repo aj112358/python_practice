9ca41d41c5b659497e684f1822c8cae2b2ac7f3b
AJ <amarjits1@hotmail.com>
2021-03-29 16:05:11 -0600

    implemented 'key' to define how sorting should be done

--- Sorting/decorated_merge_sort.py
+++ Sorting/decorated_merge_sort.py
@@ -6,11 +6,8 @@ Created By: AJ Singh
 Date: March 29, 2021
 """
 
-
 from merge_sort import merge_sort
 
-
-# class SortingDecorator:
 
 class _Item:
     """Lightweight composite to store key-value pairs."""
@@ -25,14 +22,33 @@ class _Item:
 
 
 def decorated_merge_sort(data, key=None):
-    """Demonstration of the 'decorate-sort-undecorate' design pattern."""
+    """Sort a sequence using merge-sort, and an optional 'key' to define how sorting should be done."""
 
     if key is not None:
         for j in range(len(data)):
             data[j] = _Item(k=key(data[j]), v=data[j])
 
-    merge_sort(data)
+    merge_sort(data)  # Can use any sorting function.
 
     if key is not None:
         for j in range(len(data)):
             data[j] = data[j]._value
+
+
+if __name__ == "__main__":
+    seq = ['red', 'green', 'blue', 'cyan', 'magenta', 'yellow']
+
+    print("Original:")
+    print(*seq, sep=" ")
+
+    decorated_merge_sort(seq)
+    print("\nSorted (via <):")
+    print(*seq, sep=" ")
+
+    decorated_merge_sort(seq, key=len)
+    print("\nSorted (via length):")
+    print(*seq, sep=" ")
+
+    decorated_merge_sort(seq, key=lambda x: x[-1])
+    print("\nSorted (via last character in string):")
+    print(*seq, sep=" ")
