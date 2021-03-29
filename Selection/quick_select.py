2d71b720af611ddbfe5263489a9f5d25c5ce06cc
AJ <amarjits1@hotmail.com>
2021-03-29 17:12:17 -0600

    implementing algorithm to find k-th smallest element

---
+++ Selection/quick_select.py
@@ -0,0 +1,11 @@
+"""An implementation to find the k-th smallest element in a collection.
+
+Created By: AJ Singh
+Date: March 29, 2021
+"""
+
+
+def quick_select(s, k):
+    """Return the k-th smallest element from the list 's'."""
+
+
