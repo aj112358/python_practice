5ac237fd3d97dcf2f783e63934560b3111a4aec8
AJ <amarjits1@hotmail.com>
2021-03-29 15:58:21 -0600

    wrote code for applying the key function for sorting

--- Sorting/decorated_merge_sort.py
+++ Sorting/decorated_merge_sort.py
@@ -7,22 +7,32 @@ Date: March 29, 2021
 """
 
 
-class SortingDecorator:
-
-    class _Item:
-        """Lightweight composite to store key-value pairs."""
-        __slots__ = '_key', '_value'
-
-        def __init__(self, k, v):
-            self._key = k
-            self._value = v
-
-        def __lt__(self, other):
-            return self._key < other._key
+from merge_sort import merge_sort
 
 
+# class SortingDecorator:
+
+class _Item:
+    """Lightweight composite to store key-value pairs."""
+    __slots__ = '_key', '_value'
+
+    def __init__(self, k, v):
+        self._key = k
+        self._value = v
+
+    def __lt__(self, other):
+        return self._key < other._key
 
 
 def decorated_merge_sort(data, key=None):
     """Demonstration of the 'decorate-sort-undecorate' design pattern."""
 
+    if key is not None:
+        for j in range(len(data)):
+            data[j] = _Item(k=key(data[j]), v=data[j])
+
+    merge_sort(data)
+
+    if key is not None:
+        for j in range(len(data)):
+            data[j] = data[j]._value
