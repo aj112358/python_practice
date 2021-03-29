1bc9a75453a8146efdd3058c64f45bcd1278557d
AJ <amarjits1@hotmail.com>
2021-03-29 15:51:48 -0600

    created nested _Item class to hold key-value pairs

--- Sorting/decorated_merge_sort.py
+++ Sorting/decorated_merge_sort.py
@@ -7,5 +7,22 @@ Date: March 29, 2021
 """
 
 
+class SortingDecorator:
+
+    class _Item:
+        """Lightweight composite to store key-value pairs."""
+        __slots__ = '_key', '_value'
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __lt__(self, other):
+            return self._key < other._key
+
+
+
+
 def decorated_merge_sort(data, key=None):
     """Demonstration of the 'decorate-sort-undecorate' design pattern."""
+
